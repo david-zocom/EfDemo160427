107f1dfce96371b2c13954f673dda7061466ce9d
David Andersson <david.andersson@alumni.chalmers.se>
2016-04-27 13:43:13 +0200

    DefaultController

---
+++ Demo1/Demo1/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo1.Models;
+
+namespace Demo1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            using (NumbersContext context = new NumbersContext())
+            {
+                int count = context.Numbers.Count();
+                ViewBag.Count = count;
+                context.Numbers.Add(new NumbersModel()
+                {
+                    Number = count
+                });
+                context.SaveChanges();
+                var lista = from x in context.Numbers
+                            select x.Number;
+                ViewBag.Lista = lista.ToList();
+            }
+
+            return View();
+        }
+
+        public ActionResult Bugged()
+        {
+            string x = null;
+            string y = x.Trim();
+            return View();
+        }
+    }
+}
