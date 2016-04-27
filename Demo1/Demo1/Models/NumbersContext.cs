188647a1c6ce1170575f014de4c62b33c9f475ef
David Andersson <david.andersson@alumni.chalmers.se>
2016-04-27 11:30:53 +0200

    First version

---
+++ Demo1/Demo1/Models/NumbersContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Demo1.Models
+{
+    public class NumbersContext : DbContext
+    {
+        public NumbersContext() : base() { }
+
+        public DbSet<NumbersModel> Numbers { get; set; }
+
+    }
+}
---
+++ Demo1/Demo1/Models/NumbersModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Demo1.Models
+{
+    [Table("DavidsNumbers")]
+    public class NumbersModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+    }
+}
